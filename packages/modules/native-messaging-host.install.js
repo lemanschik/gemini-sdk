dfcb5976e3a1e424c6b7194da593b5b35aaed625
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-03 11:30:32 +0200

    Update native-messaging-host.install.js

--- packages/modules/native-messaging-host.install.js
+++ packages/modules/native-messaging-host.install.js
@@ -1,52 +1,50 @@
+// Should get called with a glob of extensionIds that are allowed to access the native-messaging-host
+// TODO: add linux installer
 async function installConfig(extensionIds) {
   console.info(`Installing config…`)
 
   const hostName = 'com.crx.test'
   const manifest = {
     name: hostName,
-    description: 'electron-chrome-extensions test',
+    description: 'native-messaging-host test',
     path: path.join(outDir, await import("./native-messaging-host.sea-build.js").then(x=>x.binaryName)),
     type: 'stdio',
     allowed_origins: extensionIds.map((id) => `chrome-extension://${id}/`),
   }
 
   const writeManifest = async (manifestPath) => {
-    await fs.mkdir(manifestPath, { recursive: true })
-    const filePath = path.join(manifestPath, `${hostName}.json`)
-    const data = Buffer.from(JSON.stringify(manifest, null, 2))
-    await fs.writeFile(filePath, data)
-    return filePath
+    await fs.mkdir(manifestPath, { recursive: true });
+    const filePath = path.join(manifestPath, `${hostName}.json`);
+    const data = Buffer.from(JSON.stringify(manifest, null, 2));
+    await fs.writeFile(filePath, data);
+    return filePath;
   }
-
-  switch (process.platform) {
-    case 'darwin': {
-      const manifestDir = path.join(
-        os.homedir(),
-        'Library',
-        'Application Support',
-        'Electron',
-        'NativeMessagingHosts',
-      )
-      await writeManifest(manifestDir)
-      break
-    }
-    case 'win32': {
-      const manifestDir = path.join(
-        os.homedir(),
-        'AppData',
-        'Roaming',
-        'Electron',
-        'NativeMessagingHosts',
-      )
-      const manifestPath = await writeManifest(manifestDir)
+  const osSpecific = {
+    win32: [
+      'AppData',
+      'Roaming',
+    ],
+    darwin: [     
+      'Library',
+      'Application Support',
+    ]
+  };
+  const appData = osSpecific[process.platform];
+  if (appData.length) {
+    const manifestDir = path.join(
+      os.homedir(),
+      ...appData,
+      'Electron',
+      'NativeMessagingHosts',
+    );
+  
+    const manifestPath = await writeManifest(manifestDir)
+    if (process.platform.startsWith('win') {
       const registryKey = `HKCU\\Software\\Google\\Chrome\\NativeMessagingHosts\\${hostName}`
       await exec(`reg add "${registryKey}" /ve /t REG_SZ /d "${manifestPath}" /f`, {
         stdio: 'inherit',
       })
-      break
     }
-    default:
-      return
   }
 }
 
@@ -58,7 +56,6 @@ async function main() {
   }
 
   const extensionIds = extensionIdsArg.split(',')
-  await createSEA()
   await installConfig(extensionIds)
 }
 
